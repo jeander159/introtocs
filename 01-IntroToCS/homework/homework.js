f2e6021497f2fbb369d010e63127581ec8161a13
Jean Anderson <jeander_159@hotmail.com>
2022-08-04 17:04:46 -0500

    Homework Intro to CS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let arrNum = num.split('').reverse();
+  let sumaNum = 0;
+  for(let i in arrNum){
+      sumaNum = sumaNum + arrNum[i] * Math.pow(2,i);
+  }
+  return sumaNum;
+ 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let numBinario = [];
+  while(num > 0){
+    numBinario.push(num % 2);
+    num = Math.trunc(num / 2);
+  }
+  return numBinario.reverse().join('');
 }
 
 
