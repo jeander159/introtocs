a25396b98926a6cc036ae5342579dfe4e1ce0e5c
Jean Anderson <jeander_159@hotmail.com>
2022-09-02 22:02:14 -0500

    Homework 08-Algoritmos-II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,23 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
+  let arrayLeft = [];
+  let arrayRight = [];
+  let arrayEqual = [];
+ 	let pivot = array[Math.floor(Math.random()*(array.length))];
 
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > pivot){
+      	arrayRight.push(array[i])
+    }else if(array[i] < pivot){
+      	arrayLeft.push(array[i])
+    }else{
+      	arrayEqual.push(array[i])
+    }
+  }
+
+	return quickSort(arrayLeft).concat(arrayEqual).concat(quickSort(arrayRight))
 }
 
 function mergeSort(array) {
@@ -15,6 +31,28 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length<2) return array;
+  	let arrayLeft = array.slice(0,array.length/2)
+  	let arrayRigth = array.slice(array.length/2)
+    return merge(mergeSort(arrayLeft),mergeSort(arrayRigth))
+
+}
+function merge(left, right){
+  let arrayAux = [];
+  let k = 0;
+  let x = 0;
+  let y = 0;
+	
+  while(x < left.length && y < right.length){
+    if(left[x] < right[y]){
+      	arrayAux.push(left[x])
+		  	x++
+    }else{
+      	arrayAux.push(right[y])
+      	y++
+    }
+  }
+  return arrayAux.concat(left.slice(x)).concat(right.slice(y))
 }
 
 // No modificar nada debajo de esta línea
